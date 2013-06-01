c2acbcb5412dab3351c0a0ceca35c60c466d3782
Luke Bergen <LukeABergen@gmail.com>
2013-06-01 14:58:41 -0400

    trying drag n drop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-function doAlert(msg) {
-  alert(msg);
+var moving = null;
+var offsetX = 0;
+var offsetY = 0;
+
+function mouseDown(e) {
+  moving = e.target;
+  l = moving.style.left;
+  t = moving.style.top;
+  if (l === "") {
+    styles = document.defaultView.getComputedStyle(moving);
+    l = styles.left;
+    t = styles.top;
+  }
+  offsetX = parseFloat(l.split("px")[0]) - e.x;
+  offsetY = parseFloat(t.split("px")[0]) - e.y;
+}
+
+function mouseUp(e) {
+  moving = null;
+}
+
+function mouseMove(e) {
+  if (moving !== null) {
+    moving.style.top = e.y + offsetY;
+    moving.style.left = e.x + offsetX;
+  }
 }
