50d717d2ab50c021a06e6fb611121ec2bb6015e7
Luke Bergen <LukeABergen@gmail.com>
2013-06-01 15:13:43 -0400

    trying to map mouse events to touch events

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var mouseEventTypes = {
+    touchstart : "mousedown",
+    touchmove : "mousemove",
+    touchend : "mouseup"
+};
+
+for (var originalType in mouseEventTypes) {
+    document.addEventListener(originalType, function(originalEvent) {
+        e = document.createEvent("MouseEvents");
+        touch = originalEvent.changedTouches[0];
+        e.initMouseEvent(mouseEventTypes[originalEvent.type], true, true,
+                window, 0, touch.screenX, touch.screenY, touch.clientX,
+                touch.clientY, touch.ctrlKey, touch.altKey, touch.shiftKey,
+                touch.metaKey, 0, null);
+        originalEvent.target.dispatchEvent(e);
+    });
+}
+
 var moving = null;
 var offsetX = 0;
 var offsetY = 0;
